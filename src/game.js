4489b1b4588e6f63a4b1215b111d9c953c519777
Endel Dreyer <endel.dreyer@gmail.com>
2014-01-25 07:21:48 -0200

    down to gray when out of bounds

--- src/game.js
+++ src/game.js
@@ -1,25 +1,28 @@
 var game = new Phaser.Game(900, 640, Phaser.WEBGL, '', { preload: preload, create: create, update: update });
 var cursors;
-var filter;
+var filters = {};
 
 var worldVelocity = 2;
 
 
 function preload(a) {
-    game.load.image('doge', 'assets/doge.png');
-    game.load.script('filter', 'src/filters/GrayFilter.js');
+    game.load.script('gray-filter', 'src/filters/GrayFilter.js');
+    game.load.script('color-reducer-filter', 'src/filters/ColorReducerFilter.js');
     game.load.image('sky', 'assets/sky.png');
     game.load.image('ground', 'assets/platform.png');
     game.load.image('star', 'assets/star.png');
     game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 }
- 
+
 function create() {
     game.level = 1;
 
     var t = game.add.text(game.world.centerX-300, 0, "teste", { font: "65px Arial", fill: "#ff0044", align: "center" });
     cursors = game.input.keyboard.createCursorKeys();
 
+    // init filters
+    filters.grayscale = game.add.filter('GrayFilter', game.width, game.height);
+    filters.color_reducer = game.add.filter('ColorReducerFilter', game.width, game.height);
 
     player.init();
     platforms.init(game);
@@ -35,14 +38,19 @@ function create() {
     /*var doge = game.add.sprite(game.world.centerX, game.world.centerY, 'doge');
     doge.anchor.setTo(0.5,0.5);
 
-    filter = game.add.filter('GrayFilter', 800, 600);
     doge.filters = [filter];*/
 }
- 
+
+function restart() {
+}
+
 function update() {
 
     // always on top
     game.physics.collide(player.instance, platforms.group);
+
+    filters.grayscale.update();
+    filters.color_reducer.update();
 
     player.update();
 
@@ -50,8 +58,8 @@ function update() {
     platforms.refreshPosition();
 
     //filter.update();
-	
-	
+
+
 
 
 }
--- src/player.js
+++ src/player.js
@@ -5,15 +5,20 @@ var player = {
 	enabled:false,
 
 	init:function(){
-		this.instance = game.add.sprite(-50, game.world.height - 150, 'dude');
+		this.instance = game.add.sprite(100, game.world.height - 150, 'dude');
 	    this.instance.body.bounce.y = 0;
 	    this.instance.body.gravity.y = 15;
 	    this.instance.body.collideWorldBounds = false;
 
 	    this.instance.animations.add('left', [0, 1, 2, 3], 10, true);
     	this.instance.animations.add('right', [5, 6, 7, 8], 10, true);
+      this.instance.events.onOutOfBounds.add(this.onOutOfBounds);
 
 	},
+
+  onOutOfBounds: function() {
+    game.add.tween(filters.grayscale).to({ gray: 0 }, 1000).onCompleteCallback(restart).start();
+  },
 
 	update:function(){
 
@@ -23,30 +28,30 @@ var player = {
 		if(!this.enabled){
 
 			if(this.instance.body.x < 50) {
-		        this.instance.body.velocity.x = 90; 
+		        this.instance.body.velocity.x = 90;
 		    }else if(this.instance.body.x < 90) {
 		    	// REDUCING SPEED
-		        this.instance.body.velocity.x = 100 - (this.instance.body.x); 
+		        this.instance.body.velocity.x = 100 - (this.instance.body.x);
 		    }else {
 		        this.instance.body.velocity.x = 0;
 		        this.enabled = true;
 		    }
 
 		}
-		
+
 
 	    // ONLY ENABLE PLAYER JUMP AFTER THE ENTERING STAGE BEEN COMPLETED
 	    if(this.enabled){
 
 	    	 // JUMP
 	    	if (cursors.up.isDown && this.instance.body.touching.down)
-	        {  
+	        {
 	            this.instance.body.velocity.y = -600;
 	        }
 
 	    }
 
-	    
+
 
 
 	}
