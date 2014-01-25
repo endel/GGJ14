157caa3ec91f3df17adcbd3c3fb98ccd3bc4dbae
JoaoMosmannDL <jmosmann@doubleleft.com>
2014-01-25 03:23:58 -0200

    Player script moved to player.js

--- src/game.js
+++ src/game.js
@@ -1,7 +1,7 @@
 var game = new Phaser.Game(900, 640, Phaser.WEBGL, '', { preload: preload, create: create, update: update });
 var player;
 var ground;
-var cursor;
+var cursors;
 var filter;
 
 var worldVelocity = 20;
@@ -20,18 +20,10 @@ function create() {
     var t = game.add.text(game.world.centerX-300, 0, "teste", { font: "65px Arial", fill: "#ff0044", align: "center" });
     cursors = game.input.keyboard.createCursorKeys();
 
-    player = game.add.sprite(400, game.world.height - 150, 'dude');
-    player.body.bounce.y = 0.2;
-    player.body.gravity.y = 6;
-    player.body.collideWorldBounds = true;
- 	
-    //  Our two animations, walking left and right.
-    player.animations.add('left', [0, 1, 2, 3], 10, true);
-    player.animations.add('right', [5, 6, 7, 8], 10, true);
-
+    player.init();
     platforms.init(game);
     platforms.group = game.add.group();
-    ground = platforms.group.create(0, game.world.height - 64, 'ground');
+    ground = platforms.group.create(-50, game.world.height - 64, 'ground');
     ground.body.immovable = true;
 
 
@@ -43,26 +35,19 @@ function create() {
 }
  
 function update() {
+    //console.log(cursors.up.isDown,player.body.touching.down,player.body.x);
+    game.physics.collide(player.instance, platforms.group);
 
-    //console.log(screen.width);
-    //game.width = screen.width;
-
-    //game.world.centerX = game.world.centerX - worldVelocity
+    player.update();
 
     /*filter.update();*/
-    
-	game.physics.collide(player, platforms.group);
+	
 	ground.body.velocity.x = -worldVelocity;
-	player.body.velocity.x = 0;
+	
 
     platforms.refreshPosition(ground.body.x);
  
-    player.animations.play('right');
- 
     //  Allow the player to jump if they are touching the ground.
-    if (cursors.up.isDown && player.body.touching.down)
-    {
-        player.body.velocity.y = -350;
-    }
+    
 
 }
