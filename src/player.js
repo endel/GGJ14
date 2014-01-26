92fd43851551ebe036ad42ddcd77b665279d5bd7
Wagner Moschini <wagnermoschini@gmail.com>
2014-01-26 03:34:07 -0200

    fix bug?

--- src/player.js
+++ src/player.js
@@ -14,8 +14,8 @@ var player = {
 		this.instance.body.collideWorldBounds = false;
 
 		//this.instance.animations.add('left', [0,1,2,3,4,5,6,7,8,9,10], 20, true);
-		this.instance.animations.add('right', [0,1,2,3,4,5,6,7,8,9,10,11], 12, true);
-		this.instance.events.onOutOfBounds.add(this.onOutOfBounds);
+		this.instance.animations.add('right', [0,1,2,3,4,5,6,7,8,9,10,11], 15, true);
+    this.instance.events.onOutOfBounds.add(this.onOutOfBounds);
 	},
 
   onOutOfBounds: function() {
