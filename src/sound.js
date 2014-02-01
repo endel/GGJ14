3afda6f7c1c6076b44f3a515976e1c9ccb2f59e3
wagnermoschini <wagnermoschini@gmail.com>
2014-02-01 21:38:18 -0200

    fix sounds

--- src/sound.js
+++ src/sound.js
@@ -11,16 +11,16 @@ var sound = {
   },
 
   init: function() {
-    initialized = true;
+    this.initialized = true;
 
     this.base = game.add.audio('sound-base', 1, true);
     this.basssynths = game.add.audio('sound-basssynths', 1, true);
     this.highvoices = game.add.audio('sound-highvoices', 1, true);
 
 
-    this.base.play('', 0, 1, true);
-    this.basssynths.play('', 0, 0, true);
-    this.highvoices.play('', 0, 0, true);
+    this.base.play('', 0, 0.3, true);
+    this.basssynths.play('', 0, 0.3, true);
+    this.highvoices.play('', 0, 0.3, true);
 
     this.setPlaybackRate(this.playbackRate, 1);
   },
@@ -30,19 +30,31 @@ var sound = {
 
     switch(level) {
       case 1:
-        game.add.tween(this.base).to({volume: 1}, 1000).start();
-        game.add.tween(this.basssynths).to({volume: 0.1}, 1000).start();
-        game.add.tween(this.highvoices).to({volume: 0.1}, 1000).start();
+        // game.add.tween(this.base._sound).to({volume: 0.5}, 1000).start();
+        // game.add.tween(this.basssynths._sound).to({volume: 0.3}, 1000).start();
+        // game.add.tween(this.highvoices._sound).to({volume: 1}, 1000).start();
+        this.base.volume(0.3);
+        this.basssynth.volume(0.1);
+        this.highvoices.volume(0.5);
+        console.log("oi endel!");
         break;
       case 2:
-        game.add.tween(this.base).to({volume: 1}, 1000).start();
-        game.add.tween(this.basssynths).to({volume: 0.2}, 1000).start();
-        game.add.tween(this.highvoices).to({volume: 0.2}, 1000).start();
+        this.base.volume(0.4);
+        this.basssynth.volume(0.2);
+        this.highvoices.volume(0.7);
+        console.log("oi endel! 2");
+        // game.add.tween(this.base._sound).to({volume: 0.1}, 1000).start();
+        // game.add.tween(this.basssynths._sound).to({volume: 0.2}, 1000).start();
+        // game.add.tween(this.highvoices._sound).to({volume: 0.2}, 1000).start();
         break;
       case 3:
-        game.add.tween(this.base).to({volume: 1}, 1000).start();
-        game.add.tween(this.basssynths).to({volume: 1}, 1000).start();
-        game.add.tween(this.highvoices).to({volume: 1}, 1000).start();
+        console.log("oi endel! 3");
+        this.base.volume(0.6);
+        this.basssynth.volume(0.3);
+        this.highvoices.volume(1);
+        // game.add.tween(this.base._sound).to({volume: 1}, 1000).start();
+        // game.add.tween(this.basssynths._sound).to({volume: 1}, 1000).start();
+        // game.add.tween(this.highvoices._sound).to({volume: 1}, 1000).start();
         break;
     }
   },
